a531c7fa95b5a292bdb8611217e31bc3ca135d31
Tanawat Tassana <amp.tanawat@gmail.com>
2017-07-01 03:34:03 +0700

    Connect redux store to App

--- src/index.js
+++ src/index.js
@@ -1,8 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {Provider as ReduxProvider} from 'react-redux'
 import './index.css';
 import App from './App';
+import reduxStore from './app/state/store'
 //import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render((
+  <ReduxProvider store={reduxStore}>
+    <App />
+  </ReduxProvider>
+), document.getElementById('root'));
 //registerServiceWorker();

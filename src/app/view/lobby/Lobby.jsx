8feb349727c49b7ed2b848fc4a0f2ebc3324de60
Tanawat Tassana <amp.tanawat@gmail.com>
2017-07-04 04:07:56 +0700

    Create room with custom name

--- src/app/view/lobby/Lobby.jsx
+++ src/app/view/lobby/Lobby.jsx
@@ -1,23 +1,29 @@
 import React from 'react'
 import {connect} from 'react-redux'
-import {compose, withHandlers} from 'recompose'
+import {compose, withHandlers, withState} from 'recompose'
 
 import {gameActions} from '../../state/ducks/game'
 
 const Lobby = (props) => {
-  const {onCreateRoomClick} = props
+  const {onCreateRoomClick, roomName, onRoomNameChange} = props
   return (
     <div name="lobby">
       <h1>Create or Join the room</h1>
+      <input type="text" value={roomName} onChange={onRoomNameChange} />
       <button onClick={onCreateRoomClick}>Create</button>
     </div>
   )
 }
 
 const Lobby_composed = compose(
+  withState('roomName', 'setRoomName', ''),
   withHandlers({
-    onCreateRoomClick: ({createRoom}) => () => {
-      createRoom('Temp')
+    onRoomNameChange: ({setRoomName}) => (event) => {
+      const roomName = event.target.value
+      setRoomName(roomName)
+    },
+    onCreateRoomClick: ({createRoom, roomName}) => () => {
+      createRoom(roomName)
     }
   })
 )(Lobby)

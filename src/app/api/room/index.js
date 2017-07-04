fd049e37c729cfde6f05b18b0ad98384e836396d
Tanawat Tassana <amp.tanawat@gmail.com>
2017-07-05 02:49:12 +0700

    Implement how to generate 4 digits pin

--- src/app/api/room/index.js
+++ src/app/api/room/index.js
@@ -1,4 +1,19 @@
+import R from 'ramda'
 import {database} from 'firebase'
+
+function getRandomIntInclusive(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min + 1)) + min; //The maximum is inclusive and the minimum is inclusive 
+}
+
+export function generateRoomPin(random) {
+  const randomValue = random(0, 9999)
+  return R.pipe(
+    R.concat('0000'),
+    R.takeLast(4)
+  )(`${randomValue}`)
+}
 
 export function createNewRoom(roomName) {
   // 1. get available pin

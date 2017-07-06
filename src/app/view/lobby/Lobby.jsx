5a331e5bb25e3ba9073e4e1772ab2d012d40571e
Tanawat Tassana <amp.tanawat@gmail.com>
2017-07-06 18:06:42 +0700

    Fire join room

--- src/app/view/lobby/Lobby.jsx
+++ src/app/view/lobby/Lobby.jsx
@@ -5,37 +5,49 @@ import {compose, withHandlers, withState} from 'recompose'
 import {gameActions} from '../../state/ducks/game'
 
 const Lobby = (props) => {
-  const {onCreateRoomClick} = props
+  const {room,
+    onCreateRoomClick, onJoinRoomSubmit, onRoomChange} = props
   return (
     <div name="lobby">
       <h1>Create or Join the room</h1>
       <button onClick={onCreateRoomClick}
         className="button">Create</button>
-      <div className="field has-addons">
-        <p className="control">
-          <input type="text" className="input"
-            placeholder="4-Digits Pin"/>
-        </p>
-        <p className="control">
-          <a className="button is-primary">
-            Join
-          </a>
-        </p>
-      </div>
+      <form onSubmit={onJoinRoomSubmit}>
+        <div className="field has-addons">
+          <p className="control">
+            <input type="number" className="input"
+              value={room} onChange={onRoomChange}
+              placeholder="4-Digits Pin"/>
+          </p>
+          <p className="control">
+            <a className="button is-primary">
+              Join
+            </a>
+          </p>
+        </div>
+      </form>
     </div>
   )
 }
 
 const Lobby_composed = compose(
+  withState('room', 'setRoom', ''),
   withHandlers({
+    onRoomChange: ({setRoom}) => event => {
+      setRoom(event.target.value)
+    },
     onCreateRoomClick: ({createRoom}) => () => {
       createRoom()
+    },
+    onJoinRoomSubmit: ({joinRoom, room}) => () => {
+      joinRoom(room)
     }
   })
 )(Lobby)
 
 const Lobby_connected = connect(null, {
-  createRoom: gameActions.createRoom
+  createRoom: gameActions.createRoom,
+  joinRoom: gameActions.joinRoom
 })(Lobby_composed)
 
 export default Lobby_connected

6bad07c82f6a6b81fd5637b170032792f5435141
Tanawat Tassana <amp.tanawat@gmail.com>
2017-07-07 14:59:10 +0700

    Add Header styles

--- src/app/view/Header.jsx
+++ src/app/view/Header.jsx
@@ -2,18 +2,56 @@ import React from 'react'
 import {connect} from 'react-redux'
 import {compose, withProps, withHandlers} from 'recompose'
 import R from 'ramda'
+import styled from 'styled-components'
 
 import {sessionLens} from '../state/ducks/session'
 import {routingActions} from '../state/ducks/routing'
+import {blue600} from './base/colors'
+
+const Nav = styled.nav`
+  padding: 0.5rem;
+  display: flex;
+  box-shadow: 0 3px 4px 0 rgba(0,0,0,0.14), 0 3px 3px -2px rgba(0,0,0,0.12), 0 1px 8px 0 rgba(0,0,0,0.2);
+  background-color: ${blue600}
+`
+const Logo = styled.h1`
+  color: white;
+  margin-bottom: 0 !important;
+`
+const Menu = styled.div`
+  flex: 1;
+  display: flex;
+  justify-content: flex-end;
+`
+const ProfileMenuLink = styled.a`
+  display: flex;
+`
+const ProfileMenuImage = styled.img`
+  width: 32px;
+  height: 32px;
+  border-radius: 50%;
+`
 
 const Header = (props) => {
-  const {hasUser, onProfileClick} = props
+  const {hasUser, user, onProfileClick} = props
   return (
-    <nav name="header">
+    <Nav name="header">
+      <Logo className="title is-3">
+        <span className="icon">
+          <i className="fa fa-shield"></i>
+        </span>
+        <span>
+          Avalonia
+        </span>
+      </Logo>
+      <Menu>
       {hasUser && (
-        <a onClick={onProfileClick}>Profile</a>
+        <ProfileMenuLink onClick={onProfileClick}>
+          <ProfileMenuImage src={user.photoURL} />
+        </ProfileMenuLink>
       )}
-    </nav>
+      </Menu>
+    </Nav>
   )
 }
 
---
+++ src/app/view/base/colors.js
@@ -0,0 +1,5 @@
+export const blue600 = '#1E88E5'
+export const blue700 = '#1976D2'
+
+export const amber = '#FFC107'
+export const primary = amber
